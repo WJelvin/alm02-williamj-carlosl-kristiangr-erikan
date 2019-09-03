82d0d814b817b10b75c6e54f6efb76c59c309f5e
Erik Aneer <eric.aneer@gmail.com>
2019-09-03 14:44:55 +0200

    ManagedBeanTest class created. Tests for 8/11 methods written.

--- src/main/java/com/example/alm02/ManagedBean.java
+++ src/main/java/com/example/alm02/ManagedBean.java
@@ -13,6 +13,12 @@ public class ManagedBean implements Serializable {
     private int age;
     private String prediction;
 
+    public ManagedBean(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
     public ManagedBean() {
     }
     
